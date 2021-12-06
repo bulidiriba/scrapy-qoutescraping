1564356354854a226759788b7a7656bc42287b23
bulidiriba <bulidiriba@gmail.com>
2021-12-06 07:18:09 +0300

    extract all of the qoute with title, author and tag from quotestoscrape.com with XPath selector

--- qoutescraping/qoutescraping/spiders/qoute_spider.py
+++ qoutescraping/qoutescraping/spiders/qoute_spider.py
@@ -7,10 +7,22 @@ class QuoteSpider(scrapy.Spider):
         "http://quotes.toscrape.com/"
     ]
 
+    # extract the all qoutes with the writer and the tag
     def parse(self, response):
-        title = response.css("title::text").extract_first()
-        #title = response.css("title::text")[0].extract()
- 
-        yield {
-            "title" : title
-        }
+        all_div_quotes = response.xpath("//div[@class='quote']") 
+        # this returns all of the list of div with class quote, and that can be extracted in further, since extract method didn't applied here
+
+        # loop through all the div qoute
+        for quotes in all_div_quotes:
+            # get the title inside the div quote, its span tag with class text
+            title = quotes.xpath("span[@class='text']/text()").extract()
+            # get the author inside the div quote, then inside the span, then the tag with class author
+            author = quotes.xpath("span/small[@class='author']/text()").extract()
+            # get the tag inside the div quote, then inside another div, then all the anchor tag with class tag
+            tag = quotes.xpath("div/a[@class='tag']/text()").extract()
+            print("----------------#############------------------")
+            yield {
+                "title" : title,
+                "author" : author,
+                "tag" : tag
+            }
