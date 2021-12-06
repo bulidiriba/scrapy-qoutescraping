f181a1acba6e6770f1c2abe7faf47d731c9f403c
bulidiriba <bulidiriba@gmail.com>
2021-12-06 07:38:53 +0300

    define the item container and temporarily store the extracted data there in the item container

--- qoutescraping/qoutescraping/spiders/qoute_spider.py
+++ qoutescraping/qoutescraping/spiders/qoute_spider.py
@@ -1,5 +1,12 @@
 import scrapy
 
+from ..items import QoutescrapingItem # import the item class container from the items.py
+
+# The process is 
+# 1. Extracting the data with spider
+# 2. Temporaraily storing in the Container (items)
+# 3. Storing to the Database
+
 class QuoteSpider(scrapy.Spider):
     name = "quotes"
 
@@ -9,6 +16,9 @@ class QuoteSpider(scrapy.Spider):
 
     # extract the all qoutes with the writer and the tag
     def parse(self, response):
+
+        items = QoutescrapingItem() # instantiate the Qoutescrapingitem class imported from items.py
+
         all_div_quotes = response.xpath("//div[@class='quote']") 
         # this returns all of the list of div with class quote, and that can be extracted in further, since extract method didn't applied here
 
@@ -19,10 +29,12 @@ class QuoteSpider(scrapy.Spider):
             # get the author inside the div quote, then inside the span, then the tag with class author
             author = quotes.xpath("span/small[@class='author']/text()").extract()
             # get the tag inside the div quote, then inside another div, then all the anchor tag with class tag
-            tag = quotes.xpath("div/a[@class='tag']/text()").extract()
+            tags = quotes.xpath("div/a[@class='tag']/text()").extract()
             print("----------------#############------------------")
-            yield {
-                "title" : title,
-                "author" : author,
-                "tag" : tag
-            }
+
+            # add the extracted values to the item container
+            items["title"] = title
+            items["author"] = author
+            items["tags"] = tags
+
+            yield items
